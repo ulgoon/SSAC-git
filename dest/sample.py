ad0f66ca91efc91191eaccfea479162255590b20
ulgoon <ulgoon89@gmail.com>
2021-08-23 10:17:40 +0900

    feat: Create sample jupyter notebook
    To show the changes

---
+++ dest/sample.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Simple http request example
+# 
+# [Get data from starbucks](https://www.starbucks.co.kr/store/getStore.do?r=VWW6MBSUM0)
+
+# In[ ]:
+
+
+import requests
+from bs4 import BeautifulSoup
+
+
+# In[ ]:
+
+
+response = requests.get('https://www.google.com/')
+response.status_code
+
+
+# In[ ]:
+
+
+print(input('Enter some number:'))
+
+
+# [latex](http://www.ktug.org/xe/index.php?mid=KTUG_open_board&document_srl=248288)
+# 
+# 
+# $y=x+1$
+
+# In[ ]:
+
+
+while True:
+    print('hello')
+
+
+# In[ ]:
+
+
+for i in range(30):
+    print(i)
+
+
+# In[ ]:
+
+
+
+
